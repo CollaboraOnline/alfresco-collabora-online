10f027aec0186796ba3ce93b42dc68259b9bffdc
Jérémie Lesage <jeremie.lesage@jeci.fr>
2021-05-13 23:09:11 +0200

    Throw an exception if no srcurl can't be found

--- collabora-platform-extension/src/main/java/fr/jeci/collabora/alfresco/WopiDiscovery.java
+++ collabora-platform-extension/src/main/java/fr/jeci/collabora/alfresco/WopiDiscovery.java
@@ -30,8 +30,6 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.alfresco.error.AlfrescoRuntimeException;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -39,8 +37,6 @@ import org.xml.sax.SAXException;
  * Load and parse the WopiDiscovery.xml file from Collabora Online
  */
 public class WopiDiscovery {
-	private static final Log logger = LogFactory.getLog(WopiDiscovery.class);
-
 	private static final String DEFAULT_HOSTING_DISCOVERY = "/hosting/discovery";
 
 	private Document discoveryDoc;
@@ -57,9 +53,10 @@ public class WopiDiscovery {
 	}
 
 	/**
-	 * Return the srcurl for a given mimetype.
+	 * Return the srcurl for a given mimetype and action..
 	 *
 	 * @param mimeType
+	 * @param action
 	 * @return
 	 */
 	public String getSrcURL(String mimeType, String action) {
@@ -72,11 +69,14 @@ public class WopiDiscovery {
 		try {
 			return xPath.evaluate(xPathExpr, this.discoveryDoc);
 		} catch (XPathExpressionException e) {
-			logger.error("XPath Error return null", e);
+			throw new AlfrescoRuntimeException(
+					"wopi-discovery, XPath Error for mimeType=" + mimeType + " and action=" + action, e);
 		}
-		return null;
 	}
 
+	/**
+	 * Load discovery.xml from Collabora Online server
+	 */
 	private void loadDiscoveryXML() {
 		if (this.discoveryDoc != null) {
 			return;
