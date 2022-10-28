6c58c5eb70df816448afb6efa8108ece5f0f9f0f
Vincent <vincent@jeci.fr>
2022-10-28 09:21:42 +0200

    Mise à jour d'analyze pour reprendre la config de build prod du module

--- collabora-vue-component/analyze.config.js
+++ collabora-vue-component/analyze.config.js
@@ -1,6 +1,7 @@
 import { visualizer } from "rollup-plugin-visualizer";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +12,20 @@ export default defineConfig({
       title: "Collabora Vue application analysis",
     }),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "pristy-collabora-component",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
