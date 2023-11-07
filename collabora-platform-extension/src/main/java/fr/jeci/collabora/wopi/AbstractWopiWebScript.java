3171ba267048023095044ad3d8cccfa43a33025d
Jérémie Lesage <jeremie.lesage@jeci.fr>
2023-11-07 23:56:11 +0100

    add catch to log error before retry

--- collabora-platform-extension/src/main/java/fr/jeci/collabora/wopi/AbstractWopiWebScript.java
+++ collabora-platform-extension/src/main/java/fr/jeci/collabora/wopi/AbstractWopiWebScript.java
@@ -192,18 +192,23 @@ public abstract class AbstractWopiWebScript extends AbstractWebScript implements
 		RetryingTransactionHelper.RetryingTransactionCallback<Version> callback = new RetryingTransactionHelper.RetryingTransactionCallback<Version>() {
 			@Override
 			public Version execute() throws Throwable {
-				ContentWriter writer = contentService.getWriter(nodeRef, ContentModel.PROP_CONTENT, true);
+				try {
+					ContentWriter writer = contentService.getWriter(nodeRef, ContentModel.PROP_CONTENT, true);
 
-				// both streams are closed by putContent
-				writer.putContent(new BufferedInputStream(inputStream));
+					// both streams are closed by putContent
+					writer.putContent(new BufferedInputStream(inputStream));
 
-				Map<String, Serializable> versionProperties = new HashMap<>(2);
-				versionProperties.put(VersionBaseModel.PROP_VERSION_TYPE, VersionType.MINOR);
-				if (isAutosave) {
-					versionProperties.put(VersionBaseModel.PROP_DESCRIPTION, CollaboraOnlineService.AUTOSAVE_DESCRIPTION);
+					Map<String, Serializable> versionProperties = new HashMap<>(2);
+					versionProperties.put(VersionBaseModel.PROP_VERSION_TYPE, VersionType.MINOR);
+					if (isAutosave) {
+						versionProperties.put(VersionBaseModel.PROP_DESCRIPTION, CollaboraOnlineService.AUTOSAVE_DESCRIPTION);
+					}
+					versionProperties.put(CollaboraOnlineService.LOOL_AUTOSAVE, isAutosave);
+					return versionService.createVersion(nodeRef, versionProperties);
+				} catch (Exception e) {
+					logger.error("Error when writing content - retry", e);
+					throw e;
 				}
-				versionProperties.put(CollaboraOnlineService.LOOL_AUTOSAVE, isAutosave);
-				return versionService.createVersion(nodeRef, versionProperties);
 			}
 		};
 
