a222263e186845789fc9ee588cea9425ed626780
Jérémie Lesage <jeremie.lesage@jeci.fr>
2023-11-16 01:38:54 +0100

    share: un-hide exception

--- collabora-share-extension/src/main/resources/alfresco/web-extension/site-webscripts/dk/magenta/collabora-online/components/document-edit/collabora-online-edit.get.js
+++ collabora-share-extension/src/main/resources/alfresco/web-extension/site-webscripts/dk/magenta/collabora-online/components/document-edit/collabora-online-edit.get.js
@@ -7,41 +7,36 @@
         var nodeRef = url.args.nodeRef,
             connector = remote.connect("alfresco");
         //try and get the Wopi service url first
-        try {
-            var result = connector.get('/lool/host/url');
-            if (result.status.code == status.STATUS_OK) {
-                wopi_host_url = JSON.parse(result.response).lool_host_url;
-                if(wopi_host_url.charAt(wopi_host_url.length - 1) == '/')
-                    wopi_host_url = wopi_host_url.substring(0, wopi_host_url.length -1);
-                logger.log("\n\t\t wopi host url resolved: " + wopi_host_url);
-            }
-            else throw ("\t\twopi service url host returned: "+ result.status.code);
-        }catch(e){
-            logger.log('Error getting host url. (Using enpoint URL now):\n' + e);
-            wopi_host_url = remote.getEndpointURL("alfresco");
+
+        var result = connector.get('/lool/host/url');
+        if (result.status.code == status.STATUS_OK) {
+            wopi_host_url = JSON.parse(result.response).lool_host_url;
+            if(wopi_host_url.charAt(wopi_host_url.length - 1) == '/')
+                wopi_host_url = wopi_host_url.substring(0, wopi_host_url.length -1);
+            logger.log("\n\t\t wopi host url resolved: " + wopi_host_url);
+        }
+        else {
+            throw ("\t\twopi service url host returned: "+ result.status.code);
         }
 
-        try {
-            var result = connector.get('/lool/token?nodeRef=' + encodeURI(nodeRef) + '&action=edit');
-            if (result.status.code == status.STATUS_OK) {
-                var post = JSON.parse(result.response);
-                var wopi_src_url = post.wopi_src_url;
-                var fileId = nodeRef.substring(nodeRef.lastIndexOf('/') + 1);
-                var wopiFileURL = wopi_host_url + "/wopi/files/" + fileId;
-                var params = "WOPISrc=" + encodeURI(wopiFileURL) + "&lang=" + navigator.language;
-                params += "&closebutton=1";
+        var result = connector.get('/lool/token?nodeRef=' + encodeURI(nodeRef) + '&action=edit');
+        if (result.status.code == status.STATUS_OK) {
+            var post = JSON.parse(result.response);
+            var wopi_src_url = post.wopi_src_url;
+            var fileId = nodeRef.substring(nodeRef.lastIndexOf('/') + 1);
+            var wopiFileURL = wopi_host_url + "/wopi/files/" + fileId;
+            var params = "WOPISrc=" + encodeURI(wopiFileURL) + "&lang=" + navigator.language;
+            params += "&closebutton=1";
 
-                model.wopiFileURL = wopiFileURL;
-                model.iFrameURL = wopi_src_url + params;
-                model.access_token = post.access_token;
-                model.access_token_ttl = post.access_token_ttl;
-            }
-            else
-                throw "Unable to get permission token for document edit"
+            model.wopiFileURL = wopiFileURL;
+            model.iFrameURL = wopi_src_url + params;
+            model.access_token = post.access_token;
+            model.access_token_ttl = post.access_token_ttl;
         }
-        catch (err) {
-            logger.log('\n\nThere was an error retrieving the lool token:\n' + err + '\n\n')
+        else {
+            throw "There was an error retrieving the lool token:" + result.status.code
         }
+
 
         model.userId = user.id;
         model.firstName = user.firstName;
