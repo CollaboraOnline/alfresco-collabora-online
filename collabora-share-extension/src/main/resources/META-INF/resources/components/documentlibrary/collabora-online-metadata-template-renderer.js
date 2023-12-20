db92b9ad4e746cb06420ad307896016f0312a927
Cindy Piassale <cindy@jeci.fr>
2023-12-20 09:05:06 +0100

    Modification des évaluateurs
    Suite au changement de verrou, les évaluateurs ont été modifié pour que
    l'action s'affiche toujours
    Modification du banner pour afficher l'édition en cours

--- collabora-share-extension/src/main/resources/META-INF/resources/components/documentlibrary/collabora-online-metadata-template-renderer.js
+++ collabora-share-extension/src/main/resources/META-INF/resources/components/documentlibrary/collabora-online-metadata-template-renderer.js
@@ -6,10 +6,10 @@
 						propertyName : "editingBanner",
 						renderer : function collaboraEditing_renderer(record, label) {
 							var jsNode = record.jsNode, properties = jsNode.properties, html = "";
-							var editors = properties["collabora:editors"] || "";
+							var editors = properties["cm:lockOwner"] || "";
 							var txt = this.msg("details.banner.collabora-editing");
 							return '<span>' + label + '</span><span>' + txt
-									+ ' ' + editors + '</span>';
+									+ ' ' + editors.displayName + '</span>';
 						}
 					});
 })();
