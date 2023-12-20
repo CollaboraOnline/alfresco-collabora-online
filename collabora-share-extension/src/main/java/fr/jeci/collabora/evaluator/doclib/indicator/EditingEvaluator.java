7478a88ed65027e8e48d035a49ada59a34186388
Cindy Piassale <cindy@jeci.fr>
2023-12-20 09:05:06 +0100

    Modification des évaluateurs
    Suite au changement de verrou, les évaluateurs ont été modifié pour que
    l'action s'affiche toujours
    Modification du banner pour afficher l'édition en cours

--- collabora-share-extension/src/main/java/fr/jeci/collabora/evaluator/doclib/indicator/EditingEvaluator.java
+++ collabora-share-extension/src/main/java/fr/jeci/collabora/evaluator/doclib/indicator/EditingEvaluator.java
@@ -2,23 +2,23 @@ package fr.jeci.collabora.evaluator.doclib.indicator;
 
 import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.web.evaluator.BaseEvaluator;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EditingEvaluator extends BaseEvaluator {
-	private static final String ASPECT_COLLABORA_ONLINE = "collabora:CollaboraOnline";
-	private static final String PROP_START_AT = "collabora:started_at";
-	private static final String PROP_EDITORS = "collabora:editors";
+
+	private static final Logger logger = LoggerFactory.getLogger(EditingEvaluator.class);
+	private static final String LOCK_WRITE = "LOCK_WRITE";
 
 	@Override
 	public boolean evaluate(JSONObject jsonObject) {
 		try {
-			JSONArray nodeAspects = getNodeAspects(jsonObject);
-			if (nodeAspects == null) {
-				return false;
-			} else {
-				return nodeAspects.contains(ASPECT_COLLABORA_ONLINE);
+			if (logger.isDebugEnabled()) {
+				logger.debug("EditingEvalutor - jsonObject" + jsonObject.toString());
 			}
+			String lockType = (String) getProperty(jsonObject, "cm:lockType");
+			return LOCK_WRITE.equals(lockType);
 		} catch (Exception err) {
 			throw new AlfrescoRuntimeException("Failed to run UI evaluator: " + err.getMessage());
 		}
