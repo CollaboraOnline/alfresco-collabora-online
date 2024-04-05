7304be761417049afe1cc533c18d1afcdb768a8c
Cindy Piassale <cindy@jeci.fr>
2024-04-05 11:10:03 +0200

    RM 642 - Fix lang share
    https://rm.jeci.tech/issues/642

--- collabora-share-extension/src/main/resources/META-INF/resources/components/documentlibrary/document-edit-collabora-online-action-default.js
+++ collabora-share-extension/src/main/resources/META-INF/resources/components/documentlibrary/document-edit-collabora-online-action-default.js
@@ -17,7 +17,7 @@ YAHOO.Bubbling.fire("registerAction", {
 
                         var wopiFileURL = window.location.origin+"/alfresco/s/wopi/files/" + fileId;
 
-                        var frameSrcURL = wopi_src_url + "WOPISrc=" + encodeURIComponent(wopiFileURL);
+                        var frameSrcURL = wopi_src_url + "WOPISrc=" + encodeURIComponent(wopiFileURL) + "&lang=" + navigator.language;
 
                         require(["jquery"], (function ($) {
                             var form = '<form id="loleafletform" name="loleafletform" target="loleafletframe" action="' + frameSrcURL + '" method="post">' +
